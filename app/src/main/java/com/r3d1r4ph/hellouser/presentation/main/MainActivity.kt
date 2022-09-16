1b2a9d4de010fde217430c1d132cc3c5c1953bce
Armen Gevorkyan <76617495+Red1sK444@users.noreply.github.com>
2022-09-16 22:20:56 +0700

    Created register activity

--- app/src/main/java/com/r3d1r4ph/hellouser/presentation/main/MainActivity.kt
+++ app/src/main/java/com/r3d1r4ph/hellouser/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.r3d1r4ph.hellouser
+package com.r3d1r4ph.hellouser.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.r3d1r4ph.hellouser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/r3d1r4ph/hellouser/presentation/register/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.r3d1r4ph.hellouser.presentation.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.r3d1r4ph.hellouser.R
+
+class RegisterActivity : AppCompatActivity(R.layout.activity_register) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
